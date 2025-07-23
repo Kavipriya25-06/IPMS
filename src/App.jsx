1fa7bd5a0074ec0f7af2d88bd1b59836fe9d80ac
Gokulkannas <100984277+Gokulkannas@users.noreply.github.com>
2025-07-23 18:07:08 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import Inward from "./pages/Inward";
 
 // import InwardList from "./pages/InwardList";
 // import InwardList from "./pages/InwardList";
-import Inwardlist from "./pages/Inwardlist";
+import Inwardlist from "./pages/InwardList";
 import PurchaseOrder from "./pages/POTemplate";
 import Login from "./pages/Login";
 import ProtectedRoute from "./ProtectedRoute";
